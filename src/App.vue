8cc8ca7d7c9008801916ef651f0b8b70ed4c2a97
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-04-23 00:57:13 -0600

    feat(App header): add Parqueen logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+  import Button from './components/Button.vue'
+</script>
+
 <template>
-  <header class="h-20 bg-red-700 flex p-3">
-    <div class="h-auto bg-yellow-700 rounded inline-flex items-center px-1">Logo mamalon</div>
-  </header>
-  <router-view></router-view>
+  <div class='h-full w-full'>
+    <header class="grid grid-cols-3 p-3 gap-2 shadow-md shadow-gray-700 justify-center">
+      <div class="col-span-1"></div>
+      <div class="col-span-1 flex justify-center items-center">
+        <img class="rounded-full w-24" src="./assets/parqueen-log.png" />
+        <p class="text-black dark:text-white text-xl">Parqueen</p>
+      </div>
+      <!-- <div class="flex justify-end items-center"> -->
+      <!--   <Button color="primary" class="h-fit text-xl">Administración</Button> -->
+      <!-- </div> -->
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
