a7897f90a64441293db195efe4184d6aed81bef9
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-04-23 00:54:54 -0600

    feat(Components): add `Button`

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :class="[
+      'border rounded-md px-5 disabled:opacity-50 w-fit',
+      classes
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  color: 'primary' | 'secondary',
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'primary'
+})
+
+const classesDictionary = {
+  primary: 'bg-emerald-500 text-black dark:text-white border-emerald-300',
+  secondary: 'bg-gray-100 dark:bg-gray-700 text-black dark:text-white border-gray-500 dark:border-gray-600 '
+}
+
+const classes = computed(() => classesDictionary[props.color])
+</script>
