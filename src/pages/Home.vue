681902aea8851588c61c7f3466612b1b223cf278
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-04-23 00:57:41 -0600

    feat(Home page):  improve style and new design

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,40 +1,104 @@
 <script setup lang="ts">
   import Spinner from './../components/Spinner.vue'
-  import { ipcRenderer } from 'electron'
-  import { onUnmounted, watchEffect, ref } from 'vue'
+  import Button from './../components/Button.vue'
+  import { ipcRenderer, clipboard } from 'electron'
+  import { ref, reactive } from 'vue'
+  import Card from './../components/Card.vue'
+  import Message from './../components/Message.vue'
+
+  const placesDict = {
+    normal: 'Normal',
+    big: 'Grande',
+    worker: 'Trabajador',
+    special: 'Discapacitado'
+  }
 
   let isPrinting = ref(false)
+  let messageType = ref<InstanceType<typeof Message>['$props']['type']>('success')
+  let showMessage = ref(false)
+  let message = ref('')
+  let placeType = ref<TicketData['placeType']>('normal')
 
-  const stopWatch = watchEffect(() => console.log({ isPrinting: isPrinting.value }))
-
-  const submit = async () => {
-    isPrinting.value = true
-    const ticketDate = { date: new Date().toString(), id: 1, position: 'A1' }
-    await ipcRenderer.invoke('print-ticket', ticketDate)
-      .then(filename => console.log({ filename }))
-    isPrinting.value = false
+  interface Form {
+    placeType: string | null
   }
- 
-  onUnmounted(() => {
-    stopWatch()
+
+  const form = reactive<Form>({
+    placeType: null
   })
 
+  const printTickt = async () => {
+    try {
+      isPrinting.value = true
+      console.log(`Request place of type: ${placeType.value}`)
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      const ticketData = {
+        date: new Date().toISOString(),
+        id: 1,
+        position: 'A1',
+        placeType: placeType.value
+      }
+      const filename = await ipcRenderer.invoke('print-ticket', ticketData)
+      console.log(`Copying "${filename}" to clipboard`)
+      clipboard.writeText(filename, 'clipboard')
+
+      isPrinting.value = false
+
+      messageType.value = "success"
+      message.value = "Se te asigno el lugar A1"
+    } catch (err) {
+      messageType.value = 'error',
+      message.value = 'Hubo un error asignandote un lugar. Intentalo de nuevo, si el problema persiste contacta a soporte.'
+    }
+    showMessage.value = true
+    setTimeout(() => {
+      showMessage.value = false
+    }, 5000)
+  }
 </script>
 
 <template>
-  <div>
-    <div>
-
+  <Card class="m-4 h-full">
+    <div class="grid grid-cols-5 gap-3 text-black dark:text-white h-full">
+      <div class="col-span-2 gap-2 flex flex-col">
+        <p class="text-2xl">Seleccione el tipo de cajón</p>
+        <Button
+          v-for="(value, key) in placesDict"
+          :name="key"
+          :color="key === placeType ? 'primary' : 'secondary'"
+          class="w-auto text-xl"
+          @click="() => placeType = key"
+        >
+          {{ value }}
+        </Button>
+        <Button
+          class="mt-4"
+          type="button"
+          color="primary"
+          :disabled="isPrinting"
+          @click="printTickt"
+        >
+          <div class="flex justify-center items-center py-1">
+            <Spinner class="fixed" v-show="isPrinting" />
+            <p>Imprimir boleto</p>
+          </div>
+        </Button>
+      </div>
+      <div class="text-xl col-span-3 text-center">
+        <p>Bienvenido a Parqueen S.A. de C.V.</p>
+        <p>Cuida tu boleto.</p>
+        <p>Estaciónate en el lugar que el boleto indique</p>
+        <p>Respete los lugares de estacionamiento</p>
+        <p>Procure no generar conflictos</p>
+        <p>Saludos crack</p>
+      </div>
     </div>
-    <button
-      :disabled="isPrinting"
-      class="rounded bg-blue-600 text-gray-200 px-1 py-0.5 disabled:opacity-50 flex justify-center min-w-fit"
-      @click="submit"
-    >
-    <div v-show="isPrinting" class="flex justify-center">
-      <Spinner />
-    </div>
-    <p v-show="!isPrinting">Imprimir ticket</p>
-    </button>
-  </div>
+    <Message
+      v-if="showMessage"
+      class="mt-4 p-2 shadow-sm rounded-md w-fit"
+      :type="messageType"
+      :title="messageType === 'success' ? 'Cuida bien tu boleto' : 'Error'"
+      :description="message"
+    />
+  </Card>
 </template>
